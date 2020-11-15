dd48673804adf43ce77cc831e4afbfb3d4286042
EdwEd <EdwEd>
2020-11-15 17:05:05 +0300

    fileds instead properties and more "readable" code with last .Net platform "syntaxis sugar"

--- Enterprise.cs
+++ Enterprise.cs
@@ -8,54 +8,30 @@ namespace Incapsulation.EnterpriseTask
 {
     public class Enterprise
     {
-        Guid guid;
+        readonly Guid guid;
+        public Guid Guid { get; }
 
-        public Guid getGuid() { return guid; }
 
-        public Enterprise(Guid guid)
-        {
-            this.guid = guid;
-        }
+        public string Name { get; set; }
+        public DateTime EstablishDate { get; set; }
+        public TimeSpan ActiveTimeSpan  { get { return DateTime.Now - EstablishDate; } }
 
-        string name;
+        public string Inn 
+        { 
+            get { return Inn; }
+            set
+            {
+                if (Inn.Length != 10 || !Inn.All(z => char.IsDigit(z)))
+                    throw new ArgumentException();
+                Inn=value;
+            } 
+        }        
 
-        public string getName() { return name; }
-
-        public void setName(string name) { this.name = name; }
-
-        string inn;
-
-        public string getINN() { return inn; }
-
-        public void setINN(string inn)
-        {
-            if (inn.Length != 10 || !inn.All(z => char.IsDigit(z)))
-                throw new ArgumentException();
-            this.inn = inn;
-        }
-
-        DateTime establishDate;
-
-        public DateTime getEstablishDate()
-        {
-            return establishDate;
-        }
-
-        public void setEstablishDate(DateTime establishDate)
-        {
-            this.establishDate = establishDate;
-        }
-
-        public TimeSpan getActiveTimeSpan()
-        {
-            return DateTime.Now - establishDate;
-        }
-
-        public double getTotalTransactionsAmount()
+        public double GetTotalTransactionsAmount()
         {
             DataBase.OpenConnection();
             var amount = 0.0;
-            foreach (Transaction t in DataBase.Transactions().Where(z => z.EnterpriseGuid == guid))
+            foreach (Transaction t in DataBase.Transactions().Where(z => z.EnterpriseGuid == Guid))
                 amount += t.Amount;
             return amount;
         }
